107be6d094cd841c28b98000195dd1a48691a491
dhruvbhardwaj14 <dhruv9891891827@gmail.com>
2023-11-05 01:53:49 +0530

    IAmPoor Ios Application

---
+++ IAmPoor/IAmPoor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAmPoor
+//
+//  Created by Dhruv Bhardwaj on 22/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
