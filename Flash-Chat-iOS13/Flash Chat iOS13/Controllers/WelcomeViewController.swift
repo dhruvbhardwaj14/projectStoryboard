1e93f786a8622be5d7c9b7a8b18a5d5b0b0a1681
dhruvbhardwaj14 <dhruv9891891827@gmail.com>
2023-11-05 01:48:24 +0530

    flashChat Ios Application

---
+++ Flash-Chat-iOS13/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WelcomeViewController.swift
+//  Flash Chat iOS13
+//
+//  Created by Angela Yu on 21/10/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.titleLabel.adjustsFontSizeToFitWidth=true
+        titleLabel.text=""
+        var charIndx=0.0;
+        let titleText=K.appName
+        for letter in titleText{
+            Timer.scheduledTimer(withTimeInterval: 0.1*charIndx, repeats: false){(timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndx+=1
+        }
+                    
+    }
+    
+
+}
