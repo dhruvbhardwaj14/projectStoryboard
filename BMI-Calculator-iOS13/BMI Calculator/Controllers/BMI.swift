5d6685b82e53b02dd39daa7f0ddda8a5a5cd1b23
dhruvbhardwaj14 <dhruv9891891827@gmail.com>
2023-11-05 01:53:09 +0530

    BMI Calculator Ios Application

---
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Dhruv Bhardwaj on 29/10/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+struct BMI{
+    let value:Float
+    let advice:String
+    let color:UIColor
+}
---
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/CalculateViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Angela Yu on 21/08/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class CalculateViewController: UIViewController {
+    
+    var calculatorBrain=CalculatorBrain()
+
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    @IBOutlet weak var heightLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func heightSliderChange(_ sender: UISlider) {
+        heightLabel.text=String(format:"%.2f",sender.value)+"m"
+    }
+    @IBAction func weightSliderChange(_ sender: UISlider) {
+        weightLabel.text="\(Int(sender.value))Kg"
+    }
+    
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
+        let height=heightSlider.value
+        let weight=weightSlider.value
+        
+        calculatorBrain.calculateBmi(height:height,weight:weight)
+        self.performSegue(withIdentifier: "goToResult", sender:  self)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier=="goToResult"{
+            let destinationVC=segue.destination as! ResultViewController
+            destinationVC.bmiValue=calculatorBrain.getBmiValue()
+            destinationVC.advice=calculatorBrain.getAdvice()
+            destinationVC.color=calculatorBrain.getColor()
+        }
+    }
+}
+
